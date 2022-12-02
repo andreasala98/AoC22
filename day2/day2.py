788d14c0779073cd78f57b83d5fd5963afa582bf
Andrea Sala <andrea.sala98@gmail.com>
2022-12-02 22:56:30 +0100

    Day 2 solution

---
+++ day2/day2.py
@@ -0,0 +1,28 @@
+"""Solutions to the day 2 problem"""
+import os, pathlib
+
+scores = {
+    # rock       #paper     #scissors
+    'A X': 1+3, 'A Y': 2+6, 'A Z': 3+0, #opp. rock
+    'B X': 1+0, 'B Y': 2+3, 'B Z': 3+6, #opp. paper
+    'C X': 1+6, 'C Y': 2+0, 'C Z': 3+3, #opp. scissors
+}
+
+scores_2 = {
+    # lose      #draw        #win
+    'A X': 3+0, 'A Y': 1+3, 'A Z': 2+6, #opp. rock
+    'B X': 1+0, 'B Y': 2+3, 'B Z': 3+6, #opp. paper
+    'C X': 2+0, 'C Y': 3+3, 'C Z': 1+6, #opp. scissors
+}
+
+file_path = os.path.join(pathlib.Path(__file__).parent.absolute(), 'data.in')
+with open(file_path, 'r') as f:
+    lines = f.read().split('\n')
+print(sum([scores[line] for line in lines]))
+print(sum([scores_2[line] for line in lines]))
+
+
+
+
+
+
