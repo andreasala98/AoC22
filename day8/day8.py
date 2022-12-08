62c5a97e7c829340f5b76ae9c1585b7a320f43a0
Andrea Sala <andrea.sala98@gmail.com>
2022-12-08 23:37:21 +0100

    Fixed linting

--- day8/day8.py
+++ day8/day8.py
@@ -4,26 +4,26 @@ import os
 import pathlib
 import numpy as np
 
-def is_visible(data, i ,j):
+def is_visible(matrix, i ,j):
     """Check if a tree is visible from outside the grid."""
-    if i in [0, len(data)-1] or j in [0, len(data)-1]:
+    if i in [0, len(matrix)-1] or j in [0, len(matrix)-1]:
         return True
-    not_vis = max(data[:i, j])>=data[i,j] and max(data[i+1:, j])>=data[i,j] and \
-        max(data[i, :j])>=data[i,j] and max(data[i, j+1:])>=data[i,j]
+    not_vis = max(matrix[:i, j])>=matrix[i,j] and max(matrix[i+1:, j])>=matrix[i,j] and \
+        max(matrix[i, :j])>=matrix[i,j] and max(matrix[i, j+1:])>=matrix[i,j]
     return not not_vis
 
-def get_scenic_score(data, i, j):
+def get_scenic_score(matrix, i, j):
     """Calculate scenic score for a specific element in the matrix"""
 
-    visions = [data[i+1:, j], data[:i, j][::-1], data[i, :j][::-1], data[i, j+1:]]
+    visions = [matrix[i+1:, j], matrix[:i, j][::-1], matrix[i, :j][::-1], matrix[i, j+1:]]
     multipliers = np.zeros(shape=(4,), dtype=int)
 
     for k, vision in enumerate(visions):
-        for n in vision:
+        for vis in vision:
             multipliers[k] += 1
-            if n>=data[i,j]:
+            if vis>=matrix[i,j]:
                 break
-    
+
     return np.prod(multipliers)
 
 file_path = os.path.join(pathlib.Path(__file__).parent.absolute(), 'data.in')
@@ -31,8 +31,8 @@ file_path = os.path.join(pathlib.Path(__file__).parent.absolute(), 'data.in')
 with open(file_path, 'r', encoding='utf-8') as f:
     data = np.array([[int(num) for num in line] for line in  f.read().split('\n')])
 
-
-print(sum([is_visible(data, i, j) for i in range(len(data)) for j in range(len(data))]))
-print(max([get_scenic_score(data, i, j) for i in range(1,len(data)-1) for j in range(1,len(data)-1)]))
-
-#2211840 too high
+result_1 = [is_visible(data, i, j) for i in range(len(data)) for j in range(len(data))]
+print(sum(result_1))
+result_2 = [get_scenic_score(data, i, j) for i in range(1,len(data)-1)
+                                     for j in range(1,len(data)-1)]
+print(max(result_2))
